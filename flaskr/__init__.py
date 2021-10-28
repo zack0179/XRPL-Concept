dc457d36a49d3f99e296f8a014c5563b5605cfa9
Zac <zack0179@gmail.com>
2021-10-27 22:35:32 -0400

    init file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -9,7 +9,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get("SECRET_KEY"),
-        DATABASE=os.environ.get("DATABASE_URL"),
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+        #DATABASE=os.environ.get("DATABASE_URL"),
     )
 
     if test_config is None:
